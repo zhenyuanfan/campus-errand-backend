bf668ebe863870556c17aa0ace41e0e96564b0f7
jiaowolf <2249472040@qq.com>
2025-11-27 19:20:06 +0800

    项目初始化

---
+++ src/main/java/com/yuan/campuserrandbackend/CampusErrandBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yuan.campuserrandbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusErrandBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusErrandBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuan/campuserrandbackend/CampusErrandBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuan.campuserrandbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusErrandBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
